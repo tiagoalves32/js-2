11b7073a6f32ce7ce0bf49e686140e688a951090
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-06-06 16:57:32 -0300

    Criei a pasta aula

---
+++ aulas/aula 07/index.js
@@ -0,0 +1,25 @@
+/*
+var idade = 10
+console.log(`Você tem ${idade} anos.`)
+if(idade < 16) {
+    console.log(`Não vota`)
+} else if (idade < 18 || idade > 65) {
+    console.log(`Voto opcional`)
+} else {
+    console.log(`Voto obrigatório`)
+}
+*/
+
+/*
+var idade = 80
+if (idade <= 17) {
+    console.log(`Não pode dirigir`)
+} else if (idade < 18 || idade > 75) {
+    console.log(`Não pode dirigir`)
+} else {
+    console.log(`Está habiitado à diigir`)
+}
+*/
+
+        
+
