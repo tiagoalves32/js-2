8106d65c32ec09cf26607e64f9fb145b795aac37
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-06-07 17:40:44 -0300

    Criei o teste 6.2

--- testes/teste-06/teste-6.2/script.js
+++ testes/teste-06/teste-6.2/script.js
@@ -1,1 +1,21 @@
-alert('olá!')
+function carregar() {
+    var mensagem = window.document.querySelector('#msg')
+    var img = window.document.querySelector('#imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12 ) {
+        // BOM DIA!
+        img.src = 'manha.png.png'
+        document.body.style.background = '#bfa220'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = 'tarde.png.png'
+        document.body.style.background = '#f58a05'
+    } else {
+        // BOA NOITE!
+        img.src = 'noite.png.png'
+        document.body.style.background = '#002860'
+    }
+}
+
