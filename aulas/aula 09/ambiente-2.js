5adb01c96b3e3a706187334f1f199c2cd390704f
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-06-12 10:21:12 -0300

    criei aula 09

---
+++ aulas/aula 09/ambiente-2.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c ++
+}
+*/
+
+console.log('Vai começar...')
+for (var c = 1; c <= 5; c ++) {
+    console.log(c)
+}
+console.log('FIM!...')
